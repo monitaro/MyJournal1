e0fec3648848baf8bb16bb3c08e4fd7fe428e4a3
Dennis Piecha <d.piecha@ymail.com>
2017-11-20 17:24:10 +0100

    clean up & bug fixes

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,22 @@ var dateFormat = require('dateformat')
 
 var Post = require('../models/post')
 
+router.get('/', function (req, res) {
+
+    Post.find({}, function (err, posts) {
+        var data = posts
+        for (let index = 0; index < data.length; index++) {
+            var test = data[index].date
+            data[index].date2 = dateFormat((data[index].date), "dddd, mmmm dS, yyyy, h:MM:ss TT");
+
+
+        }
+        res.render('home', {
+            posts: data
+        })
+    })
+})
+
 router.get('/add-post', function (req, res, next) {
     res.render('post')
 })
@@ -23,15 +39,21 @@ router.post('/add-post', function (req, res, next) {
 })
 
 router.get('/edit-post/:id', function (req, res, next) {
-    Post.findOne({ "_id": new ObjectId(req.params.id) }, function (err, post) {
+    Post.findOne({
+        "_id": new ObjectId(req.params.id)
+    }, function (err, post) {
         post.date2 = dateFormat(post.date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-        res.render('edit', { post: post })
+        res.render('edit', {
+            post: post
+        })
     })
     // res.render('post')
 })
 
 router.post('/edit-post/:id', function (req, res, next) {
-    Post.findOne({ "_id": new ObjectId(req.params.id) }, function (err, post) {
+    Post.findOne({
+        "_id": new ObjectId(req.params.id)
+    }, function (err, post) {
         post.title = req.body.title || ""
         post.content = req.body.content || ""
         post.date = new Date(req.body.date || new Date().toString())
@@ -44,14 +66,14 @@ router.post('/edit-post/:id', function (req, res, next) {
     })
 })
 
-server.get("/img", function(req, res) {
+router.get("/img", function (req, res) {
     var img = new Buffer(req.body.img, 'base64');
 
-   res.writeHead(200, {
-     'Content-Type': 'image/png',
-     'Content-Length': img.length
-   });
-   res.end(img); 
+    res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': img.length
+    });
+    res.end(img);
 });
 
 module.exports = router
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var dateFormat = require('dateformat')
 
 var app = express()
 
-mongoose.connect('mongodb://localhost:27017/journaldb')
+mongoose.connect('mongodb://localhost:27017/journaldb',{
+  useMongoClient: true,
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -23,19 +25,5 @@ app.use(express.static(path.join(__dirname, '/static/')));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
-app.get('/', function (req, res) {
-
-  Post.find({}, function (err, posts) {
-    var data = posts
-    for (let index = 0; index < data.length; index++) {
-      var test = data[index].date
-      data[index].date2 = dateFormat((data[index].date), "dddd, mmmm dS, yyyy, h:MM:ss TT");
-      
-      
-    }
-    res.render('home', { posts: data })
-  })
-})
 
 app.listen(3000)
